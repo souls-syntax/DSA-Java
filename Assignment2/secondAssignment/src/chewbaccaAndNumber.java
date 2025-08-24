912391b4bba5679b5268aea32fd7e587ad136142
Aakarsh Kashyap <souls.syntax@gmail.com>
2025-08-24 12:08:41 +0530

    chewbaccaAndNumbe Corrections

--- Assignment2/secondAssignment/src/chewbaccaAndNumber.java
+++ Assignment2/secondAssignment/src/chewbaccaAndNumber.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class chewbaccaAndNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int newNum = 0;
-        int placeValue = 1;
+        long n = input.nextLong();
+        long newNum = 0;
+        long placeValue = 1;
         while (n > 0) {
-            int temp = n%10;
+            long temp = n%10;
+            
             if(temp > (9-temp)) {
-                newNum = (newNum) + ((9-temp)*placeValue);
+                if(n<10 && temp == 9) {
+                    newNum = (newNum) + (temp*placeValue);
+                }
+                else {
+                    newNum = (newNum) + ((9-temp)*placeValue);
+                }
             }
             else {
                 newNum = (newNum) + (temp*placeValue);
