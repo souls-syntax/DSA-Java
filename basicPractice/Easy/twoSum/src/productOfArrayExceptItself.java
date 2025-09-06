b111d35066b8ba6cca4c5637a15796ec6e261963
souls-syntax <souls.syntax@gmail.com>
2025-09-06 13:26:07 +0530

    leetcode

--- basicPractice/Easy/twoSum/src/productOfArrayExceptItself.java
+++ basicPractice/Easy/twoSum/src/productOfArrayExceptItself.java
@@ -1,7 +1,7 @@
 public class productOfArrayExceptItself {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4};
-        int[] mul = MulArrX(arr);
+        int[] mul = moreOpti(arr);
         for (int i = 0; i < mul.length; i++) {
             System.out.println(mul[i]);
         }
@@ -27,5 +27,22 @@ public class productOfArrayExceptItself {
             left[i] = left[i] * right[i];
         }
         return left;
+   }
+
+   public static int[] moreOpti(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+
+        res[0] = 1;
+       for (int i = 1; i < arr.length; i++) {
+           res[i] = res[i-1] * arr[i-1];
+       }
+
+       int suf = 1;
+       for (int i = n-1; i>=0;i--) {
+           res[i] *= suf;
+           suf *= arr[i];
+       }
+       return res;
    }
 }
