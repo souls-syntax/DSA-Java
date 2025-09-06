567566d078f7034c6f66a9e5ce22bc435873bf0e
souls-syntax <souls.syntax@gmail.com>
2025-09-06 12:30:26 +0530

    leetcode

--- basicPractice/Easy/twoSum/src/findPivotIndex.java
+++ basicPractice/Easy/twoSum/src/findPivotIndex.java
@@ -1,22 +1,34 @@
 public class findPivotIndex {
     public static void main(String[] args) {
-        int[] pup = {1,7,3,6,5,6};
+        int[] pup = {1,-1,2};
         System.out.println(pivotIndex(pup));
     }
 
     public static int pivotIndex(int[] arr) {
-        int[] ari = new int[arr.length];
+        int[] ari = new int[arr.length+1];
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
+        int k = 0;
+        for (int i = 0; i < ari.length; i++) {
             ari[i] = sum;
+            sum = sum + arr[k];
+            if (k == arr.length-1) {
+                continue;
+            }
+            k++;
+
         }
-        int pk = 1;
+        for (int i = 0; i < ari.length; i++) {
+            System.out.print(ari[i] + " ");
+        }
+
+
+        int pk = 0;
 
         for (int j = 0; j < arr.length; j++) {
-            if (pk >= arr.length-1) {
+            if (pk > arr.length-1) {
                 return -1;
             }
+            //System.out.print("\n"+ arr[pk]);
             if (ari[j] == ari[ari.length-1] - (arr[pk] + ari[j])) {
                 return pk;
             }
