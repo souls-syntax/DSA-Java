7f446acf57162ee1742511b4117a75c94574dfae
souls-syntax <souls.syntax@gmail.com>
2025-09-07 11:37:22 +0530

    assignment 3

--- Assignment3/assignment3/src/Main.java
+++ Assignment3/assignment3/src/Main.java
@@ -8,9 +8,9 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = input.nextInt();
         }
-        invertedArr(arr);
+        int[] aop = Pinverse(arr);
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(aop[i] + " ");
         }
     }
 
@@ -18,7 +18,7 @@ public class Main {
         int n = arr.length;
         int l = 0;
         int h = n-1;
-
+        
         while (l<h) {
             int tem = arr[l];
             arr[l] = arr[h];
@@ -26,5 +26,17 @@ public class Main {
             l++;
             h--;
         }
+    }
+    public static int[] Pinverse(int[] arr) {
+        int n = arr.length;
+        
+        
+        int[] ari = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            int tem = arr[i];
+            ari[tem] = i;
+        }
+        return ari;
     }
 }
---
+++ Assignment3/assignment3/src/helle.java
@@ -0,0 +1,5 @@
+public class helle {
+    public static void main(String[] args) {
+        System.out.print('H');
+    }
+}
